436afcee4941b5f8aeeae34a47250b19439f68b3
Brick <brickw25@outlook.com>
2026-08-01 01:09:37 -0400

    minor setup/testing in main.py

--- main.py
+++ main.py
@@ -8,3 +8,8 @@ lwe.generate()
 
 integer_base = 81
 ds = dataset.LWEDataset(lwe, integer_base)
+loader = DataLoader(ds, batch_size=4, shuffle=True)
+src_batch, tgt_batch = next(iter(loader))
+
+salsa = model.SalsaModel(ds.vocab_size,1024, 512, 32, 8, 2, 8)
+print(salsa(src_batch, tgt_batch))
